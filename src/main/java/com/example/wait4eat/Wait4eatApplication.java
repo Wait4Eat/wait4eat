3d150ceebc17fa3376f959a04073dd502aa65994
subbni <slfkak@naver.com>
2025-04-02 16:05:59 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/wait4eat/Wait4eatApplication.java
@@ -0,0 +1,13 @@
+package com.example.wait4eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wait4eatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wait4eatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wait4eat/Wait4eatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wait4eat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wait4eatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
