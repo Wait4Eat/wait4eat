73b4a88fade6705285eac02f3184019be6a27581
subbni <slfkak@naver.com>
2025-04-03 19:21:27 +0900

    feat(notification): WaitingCalledEvent 관련 모듈 구현 (#7) - WaitingCalledEvent : 사장님이 특정 웨이팅을 호출했을 때 생성되는 도메인 이벤트 - WaitingCalledEventHandler : 해당 이벤트를 감지하여 알림(Notification)을 발행하는 리스너

---
+++ src/main/java/com/example/wait4eat/domain/waiting/handler/WaitingCalledEventHandler.java
@@ -0,0 +1,31 @@
+package com.example.wait4eat.domain.waiting.handler;
+
+import com.example.wait4eat.domain.notification.entity.Notification;
+import com.example.wait4eat.domain.notification.enums.NotificationType;
+import com.example.wait4eat.domain.notification.event.NotificationEvent;
+import com.example.wait4eat.domain.notification.service.NotificationService;
+import com.example.wait4eat.domain.waiting.event.WaitingCalledEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class WaitingCalledEventHandler {
+
+    private final NotificationService notificationService;
+    private final ApplicationEventPublisher eventPublisher;
+
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void handle(WaitingCalledEvent event) {
+        Notification notification = notificationService.create(
+                event.getUserId(),
+                NotificationType.WAITING_CALLED
+        );
+        eventPublisher.publishEvent(
+                new NotificationEvent(event.getUserId(), notification.getType())
+        );
+    }
+}
