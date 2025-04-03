5e952ae1418ecb359b1f29fec8ab08600c5a5189
이희수 <leehsdd33@gmail.com>
2025-04-03 16:50:23 +0900

    feat(auth): 회원가입 API 구현 (#6)

--- src/main/java/com/example/wait4eat/global/auth/controller/AuthController.java
+++ src/main/java/com/example/wait4eat/global/auth/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.example.wait4eat.global.auth.controller;
 
+import com.example.wait4eat.global.auth.dto.request.SignupRequest;
+import com.example.wait4eat.global.auth.dto.response.SignupResponse;
 import com.example.wait4eat.global.auth.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/api/v1/auth/signup")
+    public ResponseEntity<SignupResponse> signup(
+            @Valid @RequestBody SignupRequest request
+    ) {
+        return ResponseEntity.ok(authService.signup(request));
+    }
 }
---
+++ src/main/java/com/example/wait4eat/global/auth/dto/request/SignupRequest.java
@@ -0,0 +1,28 @@
+package com.example.wait4eat.global.auth.dto.request;
+
+import com.example.wait4eat.domain.user.enums.UserRole;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class SignupRequest {
+
+    private String email;
+    private String password;
+    private String nickname;
+    private UserRole role;
+
+    @Builder
+    private SignupRequest(
+            String email,
+            String password,
+            String nickname,
+            UserRole role
+    ) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+}
