80a4edb888fdcb47697d8dfbad0531cf0a9c3e12
이희수 <leehsdd33@gmail.com>
2025-04-03 23:59:43 +0900

    feat(auth): 이메일, 비밀번호 validation 추가 (#11)

--- src/main/java/com/example/wait4eat/global/auth/dto/request/SignupRequest.java
+++ src/main/java/com/example/wait4eat/global/auth/dto/request/SignupRequest.java
@@ -1,15 +1,33 @@
 package com.example.wait4eat.global.auth.dto.request;
 
 import com.example.wait4eat.domain.user.enums.UserRole;
+import com.example.wait4eat.global.consts.Const;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class SignupRequest {
 
+    @Email
+    @NotBlank(message = "이메일 입력은 필수입니다.")
     private String email;
+
+    @NotBlank(message = "비밃번호 입력은 필수입니다.")
+    @Size(min = 8)
+    @Pattern(
+            regexp = Const.PASSWORD_PATTERN,
+            message = "비밀번호 형식이 올바르지 않습니다."
+    )
     private String password;
+
     private String nickname;
+
+    @NotNull(message = "유저 롤 입력은 필수입니다.")
     private UserRole role;
 
     @Builder
