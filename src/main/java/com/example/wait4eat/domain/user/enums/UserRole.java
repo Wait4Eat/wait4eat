c075e5b60aed95a2c33be723d014c5feb4135b60
이희수 <leehsdd33@gmail.com>
2025-04-03 14:37:16 +0900

    refactor(auth): UserRole 이넘을 역할별 권한을 명확히 하도록 수정 (#6) - ROLE_USER, ROLE_OWNER 형태로 변경하여 일관성 유지 - 문자열을 UserRole로 변환하는 of() 메서드 추가 - Authority 클래스를 추가하여 권한 상수 관리 - 유효하지 않은 역할 입력 시 INVALID_USER_ROLE 예외 발생
    - 문자열을 UserRole로 변환하는 of() 메서드 추가
    - Authority 클래스를 추가하여 권한 상수 관리
    - 유효하지 않은 역할 입력 시 INVALID_USER_ROLE 예외 발생

--- src/main/java/com/example/wait4eat/domain/user/enums/UserRole.java
+++ src/main/java/com/example/wait4eat/domain/user/enums/UserRole.java
@@ -1,5 +1,30 @@
 package com.example.wait4eat.domain.user.enums;
 
+import com.example.wait4eat.global.exception.CustomException;
+import com.example.wait4eat.global.exception.ExceptionType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
-    USER, OWNER
+
+    ROLE_USER(Authority.USER),
+    ROLE_OWNER(Authority.OWNER);
+
+    private final String userRole;
+
+    public static UserRole of(String role) {
+        return Arrays.stream(UserRole.values())
+                .filter(r -> r.name().equalsIgnoreCase(role))
+                .findFirst()
+                .orElseThrow(() -> new CustomException(ExceptionType.INVALID_USER_ROLE));
+    }
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";
+        public static final String OWNER = "ROLE_OWNER";
+    }
 }
