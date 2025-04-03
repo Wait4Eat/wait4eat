79b510f15af21d4255f576716e9401c3ab69720f
이희수 <leehsdd33@gmail.com>
2025-04-03 16:50:23 +0900

    feat(auth): 회원가입 API 구현 (#6)

--- src/main/java/com/example/wait4eat/global/auth/service/AuthService.java
+++ src/main/java/com/example/wait4eat/global/auth/service/AuthService.java
@@ -1,13 +1,40 @@
 package com.example.wait4eat.global.auth.service;
 
+import com.example.wait4eat.domain.user.entity.User;
 import com.example.wait4eat.domain.user.repository.UserRepository;
+import com.example.wait4eat.global.auth.dto.request.SignupRequest;
+import com.example.wait4eat.global.auth.dto.response.SignupResponse;
+import com.example.wait4eat.global.exception.CustomException;
+import com.example.wait4eat.global.exception.ExceptionType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Transactional
+    public SignupResponse signup(SignupRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new CustomException(ExceptionType.USER_ALREADY_EXISTS);
+        }
+
+        String encodePassword = passwordEncoder.encode(request.getPassword());
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .nickname(request.getNickname())
+                .password(encodePassword)
+                .role(request.getRole())
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        return new SignupResponse(savedUser.getEmail(), savedUser.getNickname());
+    }
 }
