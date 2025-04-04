a0885990ca3150b53c435fb2f401752bca0f6a14
karyosome1 <ammonite0880@naver.com>
2025-04-04 09:23:36 +0900

    chore(waiting): querydsl 의존성 추가 (#5)

---
+++ src/main/java/com/example/wait4eat/domain/waiting/dto/response/WaitingResponse.java
@@ -0,0 +1,54 @@
+package com.example.wait4eat.domain.waiting.dto.response;
+
+import com.example.wait4eat.domain.waiting.entity.Waiting;
+import com.example.wait4eat.domain.waiting.enums.WaitingStatus;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class WaitingResponse {
+    private final Long storeId;
+    private final Long userId;
+    private final int peopleCount;
+    private final String status;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime calledAt;
+    private final LocalDateTime cancelledAt;
+    private final LocalDateTime enteredAt;
+
+    @Builder
+    private WaitingResponse(
+            Long storeId,
+            Long userId,
+            int peopleCount,
+            WaitingStatus status,
+            LocalDateTime createdAt,
+            LocalDateTime calledAt,
+            LocalDateTime cancelledAt,
+            LocalDateTime enteredAt
+    ) {
+        this.storeId = storeId;
+        this.userId = userId;
+        this.peopleCount = peopleCount;
+        this.status = status.toString();
+        this.createdAt = createdAt;
+        this.calledAt = calledAt;
+        this.cancelledAt = cancelledAt;
+        this.enteredAt = enteredAt;
+    }
+
+    public static WaitingResponse from(Waiting waiting) {
+        return WaitingResponse.builder()
+                .storeId(waiting.getId())
+                .userId(waiting.getUser().getId())
+                .peopleCount(waiting.getPeopleCount())
+                .status(waiting.getStatus())
+                .createdAt(waiting.getCreatedAt())
+                .calledAt(waiting.getCalledAt())
+                .cancelledAt(waiting.getCancelledAt())
+                .enteredAt(waiting.getEnteredAt())
+                .build();
+    }
+}
