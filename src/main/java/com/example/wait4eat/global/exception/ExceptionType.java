acc449e553b02901847f51a5e12fe5c7e5dcbeb8
karyosome1 <ammonite0880@naver.com>
2025-04-05 22:01:34 +0900

    feat(waiting): 웨이팅 취소 API 구현 (#21)

--- src/main/java/com/example/wait4eat/global/exception/ExceptionType.java
+++ src/main/java/com/example/wait4eat/global/exception/ExceptionType.java
@@ -46,6 +46,8 @@ public enum ExceptionType {
     WAITING_NOT_FOUND(HttpStatus.NOT_FOUND,  "해당 웨이팅을 찾을 수 없습니다."),
     NO_CURRENT_WAITING(HttpStatus.NOT_FOUND, "현재 대기 중인 웨이팅이 없습니다."),
     INVALID_WAITING_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 웨이팅 타입입니다."),
+    UNAUTHORIZED_CANCEL_WAITING(HttpStatus.FORBIDDEN, "웨이팅 취소 권한이 없습니다."),
+    ALREADY_FINISHED_WAITING(HttpStatus.BAD_REQUEST, "이미 완료 또는 취소된 웨이팅입니다."),
 
     // Server Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다.");
