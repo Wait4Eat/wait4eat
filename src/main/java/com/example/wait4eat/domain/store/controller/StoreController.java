0f473662cb7b5364a552da849a4fadd64d740baa
이희수 <leehsdd33@gmail.com>
2025-04-07 17:35:03 +0900

    feat(store): 가게 리스트 조회 API 구현 (#23)

--- src/main/java/com/example/wait4eat/domain/store/controller/StoreController.java
+++ src/main/java/com/example/wait4eat/domain/store/controller/StoreController.java
@@ -2,6 +2,7 @@ package com.example.wait4eat.domain.store.controller;
 
 import com.example.wait4eat.domain.store.dto.request.CreateStoreRequest;
 import com.example.wait4eat.domain.store.dto.response.CreateStoreResponse;
+import com.example.wait4eat.domain.store.dto.response.GetStoreListResponse;
 import com.example.wait4eat.domain.store.service.StoreService;
 import com.example.wait4eat.domain.user.enums.UserRole;
 import com.example.wait4eat.global.auth.dto.AuthUser;
@@ -10,9 +11,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +31,12 @@ public class StoreController {
             @Valid @RequestBody CreateStoreRequest request
             ) {
         return ResponseEntity.ok(storeService.create(authUser, request));
+    }
+
+    @GetMapping("/api/v1/stores")
+    public ResponseEntity<List<GetStoreListResponse>> getStoreList(
+            @AuthenticationPrincipal AuthUser authUser
+    ) {
+        return ResponseEntity.ok(storeService.getStoreList());
     }
 }
