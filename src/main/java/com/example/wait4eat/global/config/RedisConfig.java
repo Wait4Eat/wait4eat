cf47f9f80ea2298cf5778950d3daba2030c8869b
subbni <slfkak@naver.com>
2025-04-07 13:56:51 +0900

    feat(notification): Redis 기반 SSE 알림 시스템 구현 (#16)

---
+++ src/main/java/com/example/wait4eat/global/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package com.example.wait4eat.global.config;
+
+import com.example.wait4eat.domain.notification.subscriber.RedisSubscriber;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisSubscriber redisSubscriber;
+    private final ObjectMapper objectMapper;
+
+    @Value("${redis.notification-topic}")
+    private String notificationTopic;
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        // 구독 채널 등록
+        container.addMessageListener(redisSubscriber, new PatternTopic(notificationTopic));
+        return container;
+    }
+
+    @Bean
+    public ChannelTopic notificationChannelTopic() {
+        return new ChannelTopic(notificationTopic);
+    }
+}
