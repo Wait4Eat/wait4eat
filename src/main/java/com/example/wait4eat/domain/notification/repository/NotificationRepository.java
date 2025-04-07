ee06e19d83d82facaf4c801efcb40ea6afbb99fa
subbni <slfkak@naver.com>
2025-04-07 14:53:22 +0900

    feat(notification): 사용자 알림 목록 조회 API 구현 (#27)

--- src/main/java/com/example/wait4eat/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/wait4eat/domain/notification/repository/NotificationRepository.java
@@ -1,7 +1,12 @@
 package com.example.wait4eat.domain.notification.repository;
 
 import com.example.wait4eat.domain.notification.entity.Notification;
+import com.example.wait4eat.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findAllByUser(User user, Pageable pageable);
 }
--- src/main/java/com/example/wait4eat/domain/notification/service/NotificationService.java
+++ src/main/java/com/example/wait4eat/domain/notification/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.example.wait4eat.domain.notification.service;
 
+import com.example.wait4eat.domain.notification.dto.response.NotificationResponse;
 import com.example.wait4eat.domain.notification.entity.Notification;
 import com.example.wait4eat.domain.notification.enums.NotificationType;
 import com.example.wait4eat.domain.notification.repository.NotificationRepository;
@@ -8,8 +9,12 @@ import com.example.wait4eat.domain.user.repository.UserRepository;
 import com.example.wait4eat.global.exception.CustomException;
 import com.example.wait4eat.global.exception.ExceptionType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,8 +36,20 @@ public class NotificationService {
         );
     }
 
+    @Transactional(readOnly = true)
+    public List<NotificationResponse> getNotifications(Long userId, Pageable pageable) {
+        User user = getUserById(userId);
+
+        return notificationRepository.findAllByUser(user, pageable)
+                .stream()
+                .map(NotificationResponse::from)
+                .toList();
+    }
+
     private User getUserById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(ExceptionType.USER_NOT_FOUND));
     }
+
+
 }
