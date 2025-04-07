d270dadae6d3f3b862a4ef20c3428988d7d20809
이희수 <leehsdd33@gmail.com>
2025-04-07 18:06:07 +0900

    feat(store): 가게 상세 조회 API 구현 (#30)

--- src/main/java/com/example/wait4eat/domain/store/controller/StoreController.java
+++ src/main/java/com/example/wait4eat/domain/store/controller/StoreController.java
@@ -2,6 +2,7 @@ package com.example.wait4eat.domain.store.controller;
 
 import com.example.wait4eat.domain.store.dto.request.CreateStoreRequest;
 import com.example.wait4eat.domain.store.dto.response.CreateStoreResponse;
+import com.example.wait4eat.domain.store.dto.response.GetStoreDetailResponse;
 import com.example.wait4eat.domain.store.dto.response.GetStoreListResponse;
 import com.example.wait4eat.domain.store.service.StoreService;
 import com.example.wait4eat.domain.user.enums.UserRole;
@@ -11,10 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,4 +37,13 @@ public class StoreController {
     ) {
         return ResponseEntity.ok(storeService.getStoreList());
     }
+
+    @GetMapping("/api/v1/stores/{storeId}")
+    public ResponseEntity<GetStoreDetailResponse> getStoreDetail(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PathVariable Long storeId
+    ) {
+        return ResponseEntity.ok(storeService.getStoreDetail(storeId));
+    }
+
 }
