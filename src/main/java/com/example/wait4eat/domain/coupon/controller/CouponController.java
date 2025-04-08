91a187a7f89a365d4641f3479a9b5e18c3cfd622
Park-EJ <sophia5091191@gmail.com>
2025-04-08 14:26:20 +0900

    feat(coupon): 쿠폰 전체 조회 API 구현 (#12)

--- src/main/java/com/example/wait4eat/domain/coupon/controller/CouponController.java
+++ src/main/java/com/example/wait4eat/domain/coupon/controller/CouponController.java
@@ -1,16 +1,20 @@
 package com.example.wait4eat.domain.coupon.controller;
 
 import com.example.wait4eat.domain.coupon.dto.response.CreateCouponResponse;
+import com.example.wait4eat.domain.coupon.dto.response.GetAllCouponResponse;
 import com.example.wait4eat.domain.coupon.service.CouponService;
 import com.example.wait4eat.domain.user.enums.UserRole;
 import com.example.wait4eat.global.auth.dto.AuthUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,4 +30,16 @@ public class CouponController {
     ) {
         return ResponseEntity.ok(couponService.createCoupon(authUser, couponEventId));
     }
+
+    @Secured(UserRole.Authority.USER)
+    @GetMapping("/api/v1/coupons")
+    public ResponseEntity<List<GetAllCouponResponse>> getAllCoupon(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PageableDefault(page = 0, size = 10, sort = "issuedAt", direction = Sort.Direction.DESC)
+            Pageable pageable
+    ) {
+        List<GetAllCouponResponse> response = couponService.getAllCoupon(authUser, pageable);
+        return ResponseEntity.ok(response);
+    }
+
 }
