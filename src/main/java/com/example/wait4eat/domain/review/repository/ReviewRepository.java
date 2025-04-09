57619280dc918fa266a33a388e2d2caa4924ecb2
이현수 <mytime0316@naver.com>
2025-04-09 14:07:48 +0900

    remove(storewishlist): 리뷰 도메인 제거 (#24)

--- src/main/java/com/example/wait4eat/domain/review/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.wait4eat.domain.review.repository;
-
-import com.example.wait4eat.domain.review.entity.Review;
-import com.example.wait4eat.domain.waiting.entity.Waiting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    boolean existsByWaiting(Waiting waiting);
-}
