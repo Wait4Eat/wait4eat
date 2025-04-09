f69b0564c9585881b5700b7ff48ae38979ffcd8c
이현수 <mytime0316@naver.com>
2025-04-09 14:50:33 +0900

    refactor(storewishlist): 응답 값 통일 (#24)

--- src/main/java/com/example/wait4eat/domain/storewishlist/service/StoreWishlistService.java
+++ src/main/java/com/example/wait4eat/domain/storewishlist/service/StoreWishlistService.java
@@ -2,6 +2,7 @@ package com.example.wait4eat.domain.storewishlist.service;
 
 import com.example.wait4eat.domain.store.entity.Store;
 import com.example.wait4eat.domain.store.repository.StoreRepository;
+import com.example.wait4eat.domain.storewishlist.dto.response.DeleteStoreWishlistResponse;
 import com.example.wait4eat.domain.storewishlist.dto.response.StoreWishlistResponse;
 import com.example.wait4eat.domain.storewishlist.entity.StoreWishlist;
 import com.example.wait4eat.domain.storewishlist.repository.StoreWishlistRepository;
@@ -24,7 +25,7 @@ public class StoreWishlistService {
     private final StoreRepository storeRepository;
 
     @Transactional
-    public void createWishlist(Long storeId, AuthUser authUser) {
+    public StoreWishlistResponse createWishlist(Long storeId, AuthUser authUser) {
         User findUser = findUser(authUser);
         Store findStore = storeRepository.findById(storeId).orElseThrow(() -> new CustomException(ExceptionType.STORE_NOT_FOUND));
         StoreWishlist storeWishlist = StoreWishlist.builder().user(findUser).store(findStore).build();
@@ -34,14 +35,14 @@ public class StoreWishlistService {
         }
 
         try {
-            storeWishlistRepository.save(storeWishlist);
+            return StoreWishlistResponse.from(storeWishlistRepository.save(storeWishlist));
         } catch (CustomException ex) {
             throw new CustomException(ExceptionType.ALREADY_WISHLIST_STORE);
         }
     }
 
     @Transactional
-    public void deleteWishlist(Long storeWishlistsId, AuthUser authUser) {
+    public DeleteStoreWishlistResponse deleteWishlist(Long storeWishlistsId, AuthUser authUser) {
         User findUser = findUser(authUser);
         StoreWishlist findWishlist = storeWishlistRepository.findByIdOrElseThrow(storeWishlistsId);
 
@@ -50,6 +51,9 @@ public class StoreWishlistService {
         }
 
         storeWishlistRepository.delete(findWishlist);
+        return DeleteStoreWishlistResponse.builder()
+                .id(findWishlist.getId())
+                .build();
     }
 
     @Transactional(readOnly = true)
