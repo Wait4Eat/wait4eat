bd3c65ca48bedfc361b2f92c9a1ec986acebe962
karyosome1 <ammonite0880@naver.com>
2025-04-09 14:44:46 +0900

    feat(waiting): API 공통 응답 객체 적용 (#37)

--- src/main/java/com/example/wait4eat/domain/waiting/controller/WaitingController.java
+++ src/main/java/com/example/wait4eat/domain/waiting/controller/WaitingController.java
@@ -7,9 +7,10 @@ import com.example.wait4eat.domain.waiting.dto.response.*;
 import com.example.wait4eat.domain.waiting.enums.WaitingStatus;
 import com.example.wait4eat.domain.waiting.service.WaitingService;
 import com.example.wait4eat.global.auth.dto.AuthUser;
+import com.example.wait4eat.global.dto.response.PageResponse;
+import com.example.wait4eat.global.dto.response.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -26,61 +27,60 @@ public class WaitingController {
 
     @Secured(UserRole.Authority.USER)
     @PostMapping("/api/v1/stores/{storeId}/waitings")
-    public ResponseEntity<CreateWaitingResponse> createWaiting(
+    public ResponseEntity<SuccessResponse<CreateWaitingResponse>> createWaiting(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long storeId,
             @RequestBody @Valid CreateWaitingRequest request
     ) {
-        return ResponseEntity.ok(waitingService.createWaiting(authUser.getUserId(), storeId, request));
+        return ResponseEntity.ok(SuccessResponse.from(waitingService.createWaiting(authUser.getUserId(), storeId, request)));
     }
 
     @Secured(UserRole.Authority.OWNER)
     @GetMapping("/api/v1/stores/{storeId}/waitings")
-    public ResponseEntity<Page<WaitingResponse>> getWaitings(
+    public ResponseEntity<PageResponse<WaitingResponse>> getWaitings(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable long storeId,
             @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             @RequestParam(required = false) WaitingStatus status
     ) {
-        return ResponseEntity.ok(waitingService.getWaitings(authUser.getUserId(), storeId, status, pageable));
+        return ResponseEntity.ok(PageResponse.from(waitingService.getWaitings(authUser.getUserId(), storeId, status, pageable)));
     }
 
     @Secured(UserRole.Authority.USER)
     @GetMapping("/api/v1/waitings/me")
-    public ResponseEntity<MyWaitingResponse> getMyWaiting(
+    public ResponseEntity<SuccessResponse<MyWaitingResponse>> getMyWaiting(
             @AuthenticationPrincipal AuthUser authUser
     ) {
-        return ResponseEntity.ok(waitingService.getMyWaiting(authUser.getUserId()));
+        return ResponseEntity.ok(SuccessResponse.from(waitingService.getMyWaiting(authUser.getUserId())));
     }
 
     @Secured(UserRole.Authority.USER)
     @GetMapping("/api/v1/waitings/me/history")
-    public ResponseEntity<Page<MyPastWaitingResponse>> getMyPastWaitings(
+    public ResponseEntity<PageResponse<MyPastWaitingResponse>> getMyPastWaitings(
             @AuthenticationPrincipal AuthUser authUser,
             @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ) {
-        return ResponseEntity.ok(waitingService.getMyPastWaitings(authUser.getUserId(), pageable));
+        return ResponseEntity.ok(PageResponse.from(waitingService.getMyPastWaitings(authUser.getUserId(), pageable)));
     }
 
     @Secured(UserRole.Authority.USER)
     @DeleteMapping("/api/v1/waitings/{waitingId}")
-    public ResponseEntity<CancelWaitingResponse> cancelMyWaiting(
+    public ResponseEntity<SuccessResponse<CancelWaitingResponse>> cancelMyWaiting(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long waitingId
     ) {
-        return ResponseEntity.ok(waitingService.cancelMyWaiting(authUser.getUserId(), waitingId));
+        return ResponseEntity.ok(SuccessResponse.from(waitingService.cancelMyWaiting(authUser.getUserId(), waitingId)));
     }
 
     @Secured(UserRole.Authority.OWNER)
     @PatchMapping("/api/v1/waitings/{waitingId}/status")
-    public ResponseEntity<UpdateWaitingResponse> updateWaitingStatus(
+    public ResponseEntity<SuccessResponse<UpdateWaitingResponse>> updateWaitingStatus(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long waitingId,
             @RequestBody @Valid UpdateWaitingRequest updateWaitingRequest
     ) {
-        return ResponseEntity.ok(waitingService.updateWaitingStatus(authUser.getUserId(), waitingId, updateWaitingRequest));
+        return ResponseEntity.ok(SuccessResponse.from(waitingService.updateWaitingStatus(authUser.getUserId(), waitingId, updateWaitingRequest)));
     }
-
 }
 
 
