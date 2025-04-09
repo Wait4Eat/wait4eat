27e33a8c7928b2b563b629f140d7f1067c91911f
이현수 <mytime0316@naver.com>
2025-04-09 15:43:29 +0900

    feat(review): 가게 리뷰 조회 기능 (#34)

--- src/main/java/com/example/wait4eat/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/wait4eat/domain/review/controller/ReviewController.java
@@ -3,21 +3,37 @@ package com.example.wait4eat.domain.review.controller;
 import com.example.wait4eat.domain.review.dto.request.CreateReviewRequest;
 import com.example.wait4eat.domain.review.dto.response.ReviewResponse;
 import com.example.wait4eat.domain.review.service.ReviewService;
+import com.example.wait4eat.domain.user.enums.UserRole;
+import com.example.wait4eat.global.dto.response.PageResponse;
+import com.example.wait4eat.global.dto.response.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
 
+    @Secured(UserRole.Authority.USER)
     @PostMapping("/api/v1/stores/reviews")
     public ResponseEntity<ReviewResponse> createReview(@Valid @RequestBody CreateReviewRequest request) {
         ReviewResponse response = reviewService.createReview(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/api/v1/stores/{storeId}/reviews")
+    public ResponseEntity<PageResponse<ReviewResponse>> getAllReview(
+            @PathVariable Long storeId,
+            @PageableDefault(page = 0, size = 10, sort = "modifiedAt", direction = Sort.Direction.DESC) Pageable pageable
+            ) {
+        Page<ReviewResponse> reviewPage = reviewService.getAllReview(storeId, pageable);
+        return ResponseEntity.ok(PageResponse.from(reviewPage));
     }
 }
--- src/main/java/com/example/wait4eat/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/wait4eat/domain/review/repository/ReviewRepository.java
@@ -2,8 +2,14 @@ package com.example.wait4eat.domain.review.repository;
 
 import com.example.wait4eat.domain.review.entity.Review;
 import com.example.wait4eat.domain.waiting.entity.Waiting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     boolean existsByWaiting(Waiting waiting);
+
+    @Query("SELECT r FROM Review r JOIN r.waiting w WHERE w.store.id = :storeId")
+    Page<Review> getAllByStoreId(Long storeId, Pageable pageable);
 }
