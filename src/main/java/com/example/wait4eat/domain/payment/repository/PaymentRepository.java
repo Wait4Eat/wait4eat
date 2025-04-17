3a6fe8826093bf60f30a6e2826fbf633be469bfd
이현수 <mytime0316@naver.com>
2025-04-17 17:42:01 +0900

    feat(dashboard): 스프링 배치로 대시보드 생성 (#65)

--- src/main/java/com/example/wait4eat/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/wait4eat/domain/payment/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.example.wait4eat.domain.payment.repository;
 import com.example.wait4eat.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Long sumSalesByDate(LocalDate yesterday);
 }
--- src/main/java/com/example/wait4eat/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/wait4eat/domain/user/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.example.wait4eat.domain.user.repository;
 import com.example.wait4eat.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
+
+    Long countByLoginDate(LocalDate yesterday);
 }
