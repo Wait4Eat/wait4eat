aff6e49da1b6d488a987ba81a34b6db27f5540b9
이현수 <mytime0316@naver.com>
2025-04-17 19:39:42 +0900

    feat(dashboard): 대시보드 생성에 필요한 로직, 메서드 추가 (#65)

--- src/main/java/com/example/wait4eat/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/wait4eat/domain/payment/repository/PaymentRepository.java
@@ -2,9 +2,16 @@ package com.example.wait4eat.domain.payment.repository;
 
 import com.example.wait4eat.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    @Query("""
+    SELECT COALESCE(SUM(p.amount), 0)
+    FROM Payment p
+    WHERE FUNCTION('DATE', p.createdAt) = :date
+      AND p.status = com.example.wait4eat.domain.payment.enums.PaymentStatus.PAID
+    """)
     Long sumSalesByDate(LocalDate yesterday);
 }
