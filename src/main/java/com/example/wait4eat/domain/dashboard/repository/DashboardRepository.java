24fdc2185b64a43cfd92a1b86f681360d6da9ad3
이현수 <mytime0316@naver.com>
2025-04-17 19:37:59 +0900

    refactor(dashboard): repository 패키지 안으로 이동 (#65)

--- src/main/java/com/example/wait4eat/domain/dashboard/repository/DashboardRepository.java
+++ src/main/java/com/example/wait4eat/domain/dashboard/repository/DashboardRepository.java
@@ -1,4 +1,4 @@
-package com.example.wait4eat.domain.dashboard;
+package com.example.wait4eat.domain.dashboard.repository;
 
 import com.example.wait4eat.domain.dashboard.entity.Dashboard;
 import org.springframework.data.jpa.repository.JpaRepository;
