fd637455f60a68a889a954bfc4460fa859b7cc22
이현수 <mytime0316@naver.com>
2025-04-18 17:28:00 +0900

    fix(dashboard): 매출 순위 정하는 로직 수정 (#65)

--- src/main/java/com/example/wait4eat/domain/dashboard/entity/StoreSalesRank.java
+++ src/main/java/com/example/wait4eat/domain/dashboard/entity/StoreSalesRank.java
@@ -1,10 +1,7 @@
 package com.example.wait4eat.domain.dashboard.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -27,6 +24,7 @@ public class StoreSalesRank {
     @Column(nullable = false)
     private Long totalSales;
 
+    @Setter
     @Column(nullable = false)
     private int ranking;
 
