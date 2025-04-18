e92bca6a145812d5c5caf082dcaa9ea39e6a6c6d
이현수 <mytime0316@naver.com>
2025-04-18 15:18:03 +0900

    feat(dashboard): 통계기능 인기 가게 탑10 (#65)

--- src/main/java/com/example/wait4eat/global/exception/ExceptionType.java
+++ src/main/java/com/example/wait4eat/global/exception/ExceptionType.java
@@ -92,6 +92,9 @@ public enum ExceptionType {
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
     FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제에 실패했습니다."),
 
+    // Dashboard
+    DASH_BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 대시보드를 찾을 수 없습니다."),
+
     // Server Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다."),
     DATABASE_LOCK_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "일시적인 오류입니다. 재시도해 주세요."),
