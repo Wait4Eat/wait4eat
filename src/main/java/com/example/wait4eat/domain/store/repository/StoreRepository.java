d10ec2013c86e36263d6345f22f124bb4c3132d4
Park-EJ <sophia5091191@gmail.com>
2025-04-23 10:05:10 +0900

    feat(coupon_event): 동시성 제어 비관적 락 추가 (#80)

--- src/main/java/com/example/wait4eat/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/wait4eat/domain/store/repository/StoreRepository.java
@@ -1,12 +1,21 @@
 package com.example.wait4eat.domain.store.repository;
 
 import com.example.wait4eat.domain.store.entity.Store;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
     boolean existsByUserId (Long userId);
 
     Page<Store> findAll(Pageable pageable);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Store s where s.id = :storeId")
+    Optional<Store> findByIdWithPessimisticLock(Long storeId);
 }
