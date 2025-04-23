63dad28e550853e11e1c7315f7ca49b391eaeff4
subbni <slfkak@naver.com>
2025-04-23 17:49:00 +0900

    feat(notification): AWS SQS 관련 모듈 구현 (#85)

---
+++ src/main/java/com/example/wait4eat/infra/sqs/config/SqsConfig.java
@@ -0,0 +1,54 @@
+package com.example.wait4eat.infra.sqs.config;
+
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.time.Duration;
+
+@Configuration
+public class SqsConfig {
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public SqsTemplate sqsTemplate() {
+        return SqsTemplate.newTemplate(sqsAsyncClient());
+    }
+
+    @Bean
+    public SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory() {
+        return SqsMessageListenerContainerFactory
+                .builder()
+                .configure(sqsContainerOptionsBuilder ->
+                        sqsContainerOptionsBuilder
+                                .maxConcurrentMessages(10) // 컨테이너의 스레드 풀 크기
+                                .maxMessagesPerPoll(10) // 한 번의 폴링 요청으로 수신할 수 있는 최대 메시지 수를 지정
+                                .acknowledgementInterval(Duration.ofSeconds(5)) // AWS SQS 응답 간격
+                                .acknowledgementThreshold(10) // AWS SQS 응답 최소 개수
+                                .acknowledgementMode(AcknowledgementMode.ON_SUCCESS) // 소비 성공 시에만 ACK
+                )
+                .sqsAsyncClient(sqsAsyncClient())
+                .build();
+    }
+}
