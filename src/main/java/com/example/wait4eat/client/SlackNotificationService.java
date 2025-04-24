9e0f86f640305fd9c499fb8e0a093c4577841b5b
subbni <slfkak@naver.com>
2025-04-24 12:04:34 +0900

    feat(notification): Slack webhook 연동으로 Outbox 발행 일정 실패시 관리자 알림 구현 (#85)

---
+++ src/main/java/com/example/wait4eat/client/SlackNotificationService.java
@@ -0,0 +1,24 @@
+package com.example.wait4eat.client;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SlackNotificationService {
+
+    @Value("${slack.web-hook-url}")
+    private String slackWebhookUrl;
+    private RestTemplate restTemplate = new RestTemplate();
+
+    // TODO : 조금 더 구조화 필요
+    public void sendNotificationToSlack(String message) {
+        String payload = "{\"text\" : \"" + message + "\"}";
+        restTemplate.postForObject(slackWebhookUrl, payload, String.class);
+        log.info("Slack notification sent to slack webhook: {}", payload);
+    }
+}
