0bcc528d4fedabfd99bc5cd015510de24fdf180b
이현수 <mytime0316@naver.com>
2025-04-24 18:04:26 +0900

    refactor(dashboard): 스프링 배치 단계별 분리 (#65)

---
+++ src/main/java/com/example/wait4eat/domain/dashboard/batch/DashboardJobConfig.java
@@ -0,0 +1,26 @@
+package com.example.wait4eat.domain.dashboard.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DashboardJobConfig {
+    private final DashboardBatchSupport BatchSupport;
+    protected final DashboardStepConfig dashboardStepConfig;
+
+    @Bean
+    public Job dailyStatisticsJob(DashboardStatsAccumulator dashboardStatsAccumulator) {
+        return new JobBuilder("dailyStatisticsJob", BatchSupport.jobRepository)
+                .start(dashboardStepConfig.userStatsStep(dashboardStatsAccumulator))
+                .next(dashboardStepConfig.storeStatsStep(dashboardStatsAccumulator))
+                .next(dashboardStepConfig.paymentStatsStep(dashboardStatsAccumulator))
+                .next(dashboardStepConfig.saveDashboardStep(dashboardStatsAccumulator))
+                .next(dashboardStepConfig.updatePopularStoreStep())
+                .next(dashboardStepConfig.updateStoreSalesRankStep())
+                .build();
+    }
+}
