07d7f142f0f1d506fe9113dbdf39b63414a3e475
subbni <slfkak@naver.com>
2025-04-25 15:25:05 +0900

    refactor(payment): 결제 준비 메서드 리팩토링 (#90)

---
+++ src/main/java/com/example/wait4eat/domain/payment/consts/PaymentEndpoint.java
@@ -0,0 +1,8 @@
+package com.example.wait4eat.domain.payment.consts;
+
+public class PaymentEndpoint {
+
+    public final static String SUCCESS_PATH = "/api/v1/payments/success";
+    public final static String FAIL_PATH = "/api/v1/payments/fail";
+
+}
