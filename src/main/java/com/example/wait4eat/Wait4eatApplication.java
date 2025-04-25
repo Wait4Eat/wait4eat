625cf2614dff79e9040135f5987f06b92875d786
이현수 <mytime0316@naver.com>
2025-04-25 19:13:17 +0900

    fix(dashboard): 에러 수정 (#65)

--- src/main/java/com/example/wait4eat/Wait4eatApplication.java
+++ src/main/java/com/example/wait4eat/Wait4eatApplication.java
@@ -2,7 +2,6 @@ package com.example.wait4eat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
-@EntityScan(basePackages = "com.example.wait4eat.domain")
 public class Wait4eatApplication {
 
     public static void main(String[] args) {
