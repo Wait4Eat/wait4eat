f06cf424aa573e870f59b81013e8b18d0e68cbc4
subbni <slfkak@naver.com>
2025-04-30 18:10:22 +0900

    feat(payment): PrePayment 만료 스케줄러 구현 (#105)

--- src/main/java/com/example/wait4eat/domain/payment/repository/PrePaymentRepository.java
+++ src/main/java/com/example/wait4eat/domain/payment/repository/PrePaymentRepository.java
@@ -5,6 +5,8 @@ import com.example.wait4eat.domain.payment.enums.PrePaymentStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface PrePaymentRepository extends JpaRepository<PrePayment, Long> {
@@ -13,4 +15,6 @@ public interface PrePaymentRepository extends JpaRepository<PrePayment, Long> {
     Optional<PrePayment> findByOrderIdAndStatus(String orderId, PrePaymentStatus status);
 
     Optional<PrePayment> findByOrderId(String orderId);
+
+    List<PrePayment> findByStatusAndCreatedAtBefore(PrePaymentStatus prePaymentStatus, LocalDateTime threshold);
 }
