bc29a2a17b41de31bf6944064b69dc5776973030
이현수 <mytime0316@naver.com>
2025-05-01 09:50:30 +0900

    fix(review): openAi moderations api -> chat api로 변경 (#102)

--- src/main/java/com/example/wait4eat/domain/review/dto/response/ReviewResponse.java
+++ src/main/java/com/example/wait4eat/domain/review/dto/response/ReviewResponse.java
@@ -13,16 +13,27 @@ public class ReviewResponse {
     private final Long storeId;
     private final String content;
     private final double rating;
+    private final boolean isBlinded;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
     @Builder
-    private ReviewResponse(Long id, Long userId, Long storeId, String content, double rating, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    private ReviewResponse(
+            Long id,
+            Long userId,
+            Long storeId,
+            String content,
+            double rating,
+            boolean isBlinded,
+            LocalDateTime createdAt,
+            LocalDateTime modifiedAt
+    ) {
         this.id = id;
         this.userId = userId;
         this.storeId = storeId;
         this.content = content;
         this.rating = rating;
+        this.isBlinded = isBlinded;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
     }
@@ -35,6 +46,7 @@ public class ReviewResponse {
                     .storeId(review.getStoreId())
                     .content("블라인드 처리된 글입니다.")
                     .rating(review.getRating())
+                    .isBlinded(review.isBlinded())
                     .createdAt(review.getCreatedAt())
                     .modifiedAt(review.getModifiedAt())
                     .build();
@@ -45,6 +57,7 @@ public class ReviewResponse {
                     .storeId(review.getStoreId())
                     .content(review.getContent())
                     .rating(review.getRating())
+                    .isBlinded(review.isBlinded())
                     .createdAt(review.getCreatedAt())
                     .modifiedAt(review.getModifiedAt())
                     .build();
