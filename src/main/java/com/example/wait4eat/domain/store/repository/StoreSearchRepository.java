d1d14fee74afb383c478f92a22115e77d4598426
subbni <slfkak@naver.com>
2025-05-16 17:15:46 +0900

    refactor(store): ElasticSearch가 조건부로 등록되도록 설정 (#119) - properties/yml 파일에 feature.search.enabled=false로 설정 시 ElasticSearch 등록 X , 개발환경에서 Elasticsearch 없이도 정상 동작하도록 설정

--- src/main/java/com/example/wait4eat/domain/store/repository/StoreSearchRepository.java
+++ src/main/java/com/example/wait4eat/domain/store/repository/StoreSearchRepository.java
@@ -2,12 +2,14 @@ package com.example.wait4eat.domain.store.repository;
 
 import com.example.wait4eat.domain.store.dto.request.SearchStoreRequest;
 import com.example.wait4eat.domain.store.entity.StoreDocument;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@ConditionalOnProperty(name = "feature.search.enabled", havingValue = "true")
 public interface StoreSearchRepository extends ElasticsearchRepository<StoreDocument, String>, StoreSearchRepositoryCustom {
 
     Page<StoreDocument> searchStores(SearchStoreRequest request, Pageable pageable);
